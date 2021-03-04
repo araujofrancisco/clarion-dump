1fed0aa3523becbd3c78fab27574fcb8d6c7e861
Francisco Araujo <franciscoa@mcrinc.com>
2021-03-04 09:19:25 -0400

    Changes to only dump active data

--- datadump.sh
+++ datadump.sh
@@ -4,13 +4,13 @@
 exp="s/source\///g"
 
 for filename in source/*.DAT; do
-	# avoid issues when folder is empty
-	if [[ ! -e $filename ]]; then continue; fi
-	
-	echo "Processing" $filename
-	# removes source folder from filename to output data and script
-	output=$(echo "$filename" | sed -e "$exp")		
-	
-	cldump -Ss $filename > scripts/$output$SCRIPT_POS
-	cldump -S $filename > data/$output$DATA_POS
+        # avoid issues when folder is empty
+        if [[ ! -e $filename ]]; then continue; fi
+        
+        echo "Processing" $filename
+        # removes source folder from filename to output data and script
+        output=$(echo "$filename" | sed -e "$exp")              
+        
+        cldump -Ss $filename > scripts/$output$SCRIPT_POS		
+        cldump -Sd $filename > data/$output$DATA_POS
 done
